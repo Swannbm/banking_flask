a7383f43df718eea1de33218a27a876551b41486
swannbm <swann_bm@yahoo.fr>
2019-02-15 16:14:06 +0100

    review main

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 import logging
 import logging.config
-logging.config.fileConfig('bankin_flask/log.ini')
-log = logging.getLogger(__name__)
-
 from model import session as db
 from flask import Flask # pip install flask
 from flask_debugtoolbar import DebugToolbarExtension # pip install flask_debugtoolbar
 
 
+logging.config.fileConfig('bankin_flask/log.ini')
+log = logging.getLogger(__name__)
+
+
 app = Flask(__name__)
-app.debug = False
+app.debug = False # switch to True when developing
 app.config['SECRET_KEY'] = "Gustave Eiffel a construit une merveille tout d'acier et de rivets"
 toolbar = DebugToolbarExtension(app)
 
 
 @app.teardown_appcontext
 def cleanup(resp_or_exc):
+    """ usefull to make sqlalchemy working with flask """
     db.remove()
 
+import controler
 
 if __name__ == '__main__':
-    import controler
     app.add_url_rule('/', 'home', view_func=controler.home)
-    app.add_url_rule(
-            '/client/add',
-            'add_client',
-            view_func=controler.add_client,
-            methods=['POST', 'GET'])
+    app.add_url_rule('/client/add', 'add_client', view_func=controler.add_client, methods=['POST', 'GET'])
     app.add_url_rule(
             '/client/file/<int:client_id>',
             'client_file',
